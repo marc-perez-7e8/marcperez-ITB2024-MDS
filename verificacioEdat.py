f908a16889d3959d2bf8e1c117079a27aeaf8221
marc.perez.7e8 <marc.perez.7e8@gmail.com>
2024-10-14 08:54:35 +0200

    He boorado el main.py y he creado verificacioEdat.py

--- verificacioEdat.py
+++ verificacioEdat.py
@@ -5,6 +5,10 @@ Marc Pérez Juncas
 #Obtenir les dades
 from datetime import datetime
 
+day = datetime.today().day
+month = datetime.today().month
+year = datetime.today().year
+
 dia=int(input("Dia?"))
 if dia>31 or dia<1:
     print("Dia incorrecte")
@@ -14,22 +18,29 @@ mes=int(input("Mes?"))
 if mes>12 or mes<1:
     print("Mes incorrecte")
     exit()
+
 any=int(input("Any?"))
-if any > datetime.now().year:
+if any > year:
     print("Any incorrecte")
-    exit()
+
+if year-any >=16 and year-any <=65:
+    print("Pots treballar")
+else:
+    print("No pots treballar")
 
 dataNaixement =  dia,mes,any
 
 print(dataNaixement)
 
-print(datetime.now())
-#edatDia = datetime.now().day - dia
-#edatMes = datetime.now().month - mes
-#edatAny = datetime.now().year - any
+print(day,month,year)
+
+edatAny = year-any
+edatMes = month-mes
+edatDia = day-dia
 
 edatTotal = edatDia, edatMes, edatAny
-print("Tens",edatTotal)
+
+print("La teva edat es:", edatTotal)
 
 
 #Procesar les dades
