6b2f129f056c1c3220e3bdd5cd79883f9c6ff4d0
marc.perez.7e8 <marc.perez.7e8@gmail.com>
2024-10-14 09:33:27 +0200

    He boorado el main.py y he creado verificacioEdat.py

---
+++ quantitatDiesMes.py
@@ -0,0 +1,25 @@
+"""
+Marc Pérez Juncas
+14/10/24
+"""
+import datetime
+from contextlib import nullcontext
+try:
+    mes = int(input("Introdueix el mes: "))
+    any = int(input("introdueix el any: "))
+
+    if mes >=1 and mes <=12:
+        if mes in (12,10,8,7,5,3,1):
+            print("31")
+        elif mes in (11,9,6,4):
+            print("30")
+        else:
+            if (any % 4 == 0) and (any % 100 != 0):
+                print("29")
+            else:
+                print("28")
+    else:
+        print("Mes invalid")
+except ValueError:
+    print("Introdueix el mes numeric")
+
